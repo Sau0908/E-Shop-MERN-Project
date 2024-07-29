f61a4cdfb8c4c2ed9bf5763b699ac46ead96b20b
Saurabh Kashyap <125493327+Sau0908@users.noreply.github.com>
2024-07-30 00:42:40 +0530

    Update AllUser.js

--- client/src/component/User/AllUser.js
+++ client/src/component/User/AllUser.js
@@ -14,7 +14,7 @@ const AllUser = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const response = await axios.get("http://localhost:5000/api/user", {
+        const response = await axios.get("https://e-shop-mern-project.vercel.app/api/user", {
           headers: {
             Authorization: `Bearer ${token}`,
           },
